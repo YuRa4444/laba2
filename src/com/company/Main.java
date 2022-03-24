4b193ee9c1e8e2ca704ffd6b8df7cafd62afc1c0
Yura <102286821+YuRa4444@users.noreply.github.com>
2022-03-24 12:02:34 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main (String[] args){
+
+        System.out.println("Батько: Заяць Василь Романович");
+        System.out.println("Мама: Заяць Ірина Іванівна");
+
+    }
+}
+
